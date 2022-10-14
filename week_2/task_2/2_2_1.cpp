cffadb576d2659a0255370a159a0e1fb20befe77
Vladimier Stern <babonoffishial@mail.ru>
2022-10-14 12:07:36 +0300

    complete pyramid task

--- week_2/task_2/2_2_1.cpp
+++ week_2/task_2/2_2_1.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
+#include<iostream>
 
-int main(){
-    
+int main() {
+    int height;
+    std::cout << "Pyramid height:  ";
+    std::cin >> height;
+    for (int i = height-1; i >= 0; --i) { 
+        for (int j = 0; j < i; ++j) {
+            std::cout << " "; //пробельчики в начале строки
+        }
+        for (int k = 0; k < height - i; ++k){
+            std::cout << char(35);
+        }
+        std::cout << " ";
+        for (int k = 0; k < height - i; ++k){
+            std::cout << char(35);
+        }
+        std::cout << std::endl; // для перехода на новую ступень
+    }
     system("pause");
+    std::cout <<std::endl <<char(35) << std::endl;
     return 0;
 }
